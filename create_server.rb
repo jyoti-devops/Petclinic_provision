47e434a5bfb8bdcd1761ed3ff9265e2cde692bda
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2016-06-09 10:00:50 +0000

    first commit

---
+++ create_server.rb
@@ -0,0 +1,29 @@
+require 'chef/provisioning/aws_driver'
+
+with_driver 'aws'
+
+with_machine_options({
+    bootstrap_options: {
+    image_id: "ami-9a5787f9", # default for us-west-1
+    instance_type: "t2.small",
+    key_name: "arajdevops", # If not specified, this will be used and generated
+    key_path: "~/.chef/keys/arajdevops",
+    subnet_id: 'subnet-f1171486',
+    security_group_ids: 'sg-7d0bd819' 
+      },
+  use_private_ip_for_ssh: false, # DEPRECATED, use `transport_address_location`
+  transport_address_location: :public_ip, # `:public_ip` (default), `:private_ip` or `:dns`.  Defines how SSH or WinRM should find an address to communicate with the instance.
+})
+
+
+1.upto(1) do |i|
+machine "Petstore#{i}" do
+  #recipe 'Petstore'
+  tag 'ops'
+  converge true
+end
+end
+
+
+
+
